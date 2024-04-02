0fc3947610d9367106aa9af0dc57534e841a66e3
Choi YoonChan <choyc9239@gmail.com>
2024-04-02 18:24:33 +0900

    [fix] 코드 정렬 및 안 쓰는 주석 코드 제거

--- app/src/main/java/com/example/bootcchallenge/MainActivity.kt
+++ app/src/main/java/com/example/bootcchallenge/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.bootcchallenge
 
 import android.os.Bundle
-import android.view.View
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
